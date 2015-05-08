28c7fb58fc4f5154f0a0b7ce8b0084bcb7a1b008
cambon <daniel.cambon@udc.es>
2015-05-08 18:28:03 +0200

    VersionDAOImplementation implementada

--- apps/src/main/java/es/udc/fi/lbd/monuzz/id/apps/daos/VersionDAOImplementation.java
+++ apps/src/main/java/es/udc/fi/lbd/monuzz/id/apps/daos/VersionDAOImplementation.java
@@ -3,47 +3,56 @@ package es.udc.fi.lbd.monuzz.id.apps.daos;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.udc.fi.lbd.monuzz.id.apps.model.App;
 import es.udc.fi.lbd.monuzz.id.apps.model.Version;
+
 @Repository
 public class VersionDAOImplementation implements VersionDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	@Transactional(value="miTransactionManager")
 	public Long create(Version miVersion) {
-		// TODO Auto-generated method stub
-		return null;
+		Long id;
+		if (miVersion.getIdVersion()!=null){
+			throw new RuntimeException("Versión ya existente");
+		}
+		id = (Long) sessionFactory.getCurrentSession().save(miVersion);
+		return id;
 	}
 
 	@Transactional(value="miTransactionManager")
 	public void update(Version miVersion) {
-		// TODO Auto-generated method stub
+		sessionFactory.getCurrentSession().update(miVersion);
 		
 	}
 
 	@Transactional(value="miTransactionManager")
 	public void remove(Version miVersion) {
-		// TODO Auto-generated method stub
-		
+		if (miVersion.getIdVersion()==null){
+			throw new RuntimeException("Versión no existente");
+		}
+		sessionFactory.getCurrentSession().delete(miVersion);
 	}
 
 	@Transactional(value="miTransactionManager")
 	public Version findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return (Version) sessionFactory.getCurrentSession().get(Version.class, id);
 	}
 
 	@Transactional(value="miTransactionManager")
 	public List<Version> findAllByApp(App miApp) {
-		// TODO Auto-generated method stub
-		return null;
+		return miApp.getVersiones();
 	}
 
 	@Transactional(value="miTransactionManager")
 	public long getNumVersiones(App miApp) {
-		// TODO Auto-generated method stub
-		return 0;
+		return miApp.getVersiones().size();
 	}
 }

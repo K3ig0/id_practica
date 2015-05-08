7abd2913450bef6a569cc648d371621acbad19d6
cambon <daniel.cambon@udc.es>
2015-05-08 20:08:23 +0200

    AdminImplementation creación de log

--- apps/src/main/java/es/udc/fi/lbd/monuzz/id/apps/services/AdminImplementation.java
+++ apps/src/main/java/es/udc/fi/lbd/monuzz/id/apps/services/AdminImplementation.java
@@ -1,6 +1,5 @@
 package es.udc.fi.lbd.monuzz.id.apps.services;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -28,26 +27,28 @@ public class AdminImplementation implements AdminService {
 
 	public void registrarNuevoTipoApp(TipoApp miTipo) {
 		try {
-			if (miTipo==null)
-				log.error("AdminImplementation[registrarNuevoTipoApp(Clase TipoApp)] ==> tipoApp = null");
-			else {
+			if (miTipo != null) {
 				tipoAppDAO.create(miTipo);
-				log.info("AdminImplementation ==> Registrado el tipo de app: "
+				log.info("[Info]AdminImplementation[registrarNuevoTipoApp(<Clase> TipoApp)] ==> Registrado el tipo de app: "
 						+ miTipo.toString());
-			}
+			} else
+				log.error("[Error]AdminImplementation[registrarNuevoTipoApp(<Clase> TipoApp)] ==> tipoApp = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo registrar el tipo de app");
+			log.error("[Error]AdminImplementation[registrarNuevoTipoApp(<Clase> TipoApp)] ==> No se pudo registrar el tipo de app");
 			throw e;
 		}
 	}
 
 	public void borrarTipoApp(TipoApp miTipo) {
 		try {
-			tipoAppDAO.remove(miTipo);
-			log.info("AdminImplementation ==> Borrado el tipo de app: "
-					+ miTipo.toString());
+			if (miTipo != null) {
+				tipoAppDAO.remove(miTipo);
+				log.info("[Info]AdminImplementation[borrarTipoApp(<Clase> TipoApp)] ==> Borrado el tipo de app: "
+						+ miTipo.toString());
+			} else
+				log.error("[Error]AdminImplementation[borrarTipoApp(<Clase> TipoApp)] ==> tipoApp = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo borrar el tipo de app");
+			log.error("[Error]AdminImplementation[borrarTipoApp(<Clase> TipoApp)] ==> No se pudo borrar el tipo de app");
 			throw e;
 		}
 	}
@@ -55,11 +56,17 @@ public class AdminImplementation implements AdminService {
 	public TipoApp buscarTipoAppPorId(Long id) {
 		TipoApp tipoApp = null;
 		try {
-			tipoApp = tipoAppDAO.findById(id);
-			log.info("AdminImplementation ==> Encontrado el tipo de app: "
-					+ tipoApp.toString());
+			if (id != null) {
+				tipoApp = tipoAppDAO.findById(id);
+				if (tipoApp == null)
+					log.info("[Info]AdminImplementation[buscarTipoAppPorId(<Long> id)] ==> No se encontró el tipo de app");
+				else
+					log.info("[Info]AdminImplementation[buscarTipoAppPorId(<Long> id)] ==> Encontrado el tipo de app: "
+							+ tipoApp.toString());
+			} else
+				log.info("[Error]AdminImplementation[buscarTipoAppPorId(<Long> id)] ==> id = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar el tipo de app con el id: "
+			log.error("[Error]AdminImplementation[buscarTipoAppPorId(<Long> id)] ==> No se pudo encontrar el tipo de app con el id: "
 					+ id);
 			throw e;
 		}
@@ -69,11 +76,18 @@ public class AdminImplementation implements AdminService {
 	public TipoApp buscarTipoAppPorNombre(String nombre) {
 		TipoApp tipoApp = null;
 		try {
-			tipoApp = tipoAppDAO.findByNombre(nombre);
-			log.info("AdminImplementation ==> Encontrado el tipo de app: "
-					+ tipoApp.toString());
+			if (nombre != null) {
+				tipoApp = tipoAppDAO.findByNombre(nombre);
+				if (tipoApp == null)
+					log.info("[Info]AdminImplementation[buscarTipoAppPorNombre(<String> nombre)] ==> No se encontró el tipo de app con el nombre: "
+							+ nombre);
+				else
+					log.info("[Info]AdminImplementation[buscarTipoAppPorNombre(<String> nombre)] ==> Encontrado el tipo de app: "
+							+ tipoApp.toString());
+			} else
+				log.error("[Error]AdminImplementation[buscarTipoAppPorNombre(<String> nombre)] ==> nombre = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar el tipo de app con el nombre: "
+			log.error("[Error]AdminImplementation[buscarTipoAppPorNombre(<String> nombre)] ==> No se pudo encontrar el tipo de app con el nombre: "
 					+ nombre);
 			throw e;
 		}
@@ -84,9 +98,9 @@ public class AdminImplementation implements AdminService {
 		List<TipoApp> tipoApps;
 		try {
 			tipoApps = tipoAppDAO.findAll();
-			log.info("AdminImplementation ==> Recuperada a lista de tipos de app correctamente");
+			log.info("[Info]AdminImplementation[buscarTodosTipoApp()] ==> Recuperada a lista de tipos de app correctamente");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar ningún tipo de app");
+			log.error("[Error]AdminImplementation[buscarTodosTipoApp()] ==> No se pudo encontrar ningún tipo de app");
 			throw e;
 		}
 		return tipoApps;
@@ -95,19 +109,21 @@ public class AdminImplementation implements AdminService {
 	// Implementado para N niveles
 	public void registrarNuevaCategoria(Categoria miCategoria) {
 		try {
-			categoriaDAO.create(miCategoria);
-			log.info("AdminImplementation[registrarNuevaCategoria(Clase categoria)] ==> Registrada la categoría: "
-					+ miCategoria.toString());
+			if (miCategoria != null) {
+				categoriaDAO.create(miCategoria);
+				log.info("[Info]AdminImplementation[registrarNuevaCategoria(<Clase> categoria)] ==> Registrada la categoría: "
+						+ miCategoria.toString());
 
-			Set<Categoria> subCategorias = miCategoria.getSubcategorias();
+				Set<Categoria> subCategorias = miCategoria.getSubcategorias();
 
-			if (!subCategorias.isEmpty())
-				for (Categoria subCat : subCategorias) {
-					registrarNuevaCategoria(subCat);
-				}
-			
+				if (!subCategorias.isEmpty())
+					for (Categoria subCat : subCategorias) {
+						registrarNuevaCategoria(subCat);
+					}
+			} else
+				log.error("[Error]AdminImplementation[registrarNuevaCategoria(<Clase> categoria)] ==> categoria = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo registrar la categoría");
+			log.error("[Error]AdminImplementation[registrarNuevaCategoria(<Clase> categoria)] ==> No se pudo registrar la categoría");
 			throw e;
 		}
 	}
@@ -119,11 +135,15 @@ public class AdminImplementation implements AdminService {
 
 	public void borrarCategoria(Categoria miCategoria) {
 		try {
-			categoriaDAO.remove(miCategoria);
-			log.info("AdminImplementation ==> Borrada la categoría: "
-					+ miCategoria.toString() + "y sus subcategorías");
+			if (miCategoria != null) {
+				categoriaDAO.remove(miCategoria);
+				log.info("[Info]AdminImplementation[borrarCategoria(<Clase> categoria)] ==> Borrada la categoría: "
+						+ miCategoria.toString() + "y sus subcategorías");
+			} else
+				log.error("[Error]AdminImplementation[borrarCategoria(<Clase> categoria)] ==> categoria = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo borrar la categoría ni sus subcategorías");
+			log.error("[Error]AdminImplementation[borrarCategoria(<Clase> categoria)] ==> No se pudo borrar la categoría: "
+					+ miCategoria.toString() + " ni sus subcategorías");
 			throw e;
 		}
 	}
@@ -131,15 +151,17 @@ public class AdminImplementation implements AdminService {
 	public Categoria buscarCategoriaPorId(Long id) {
 		Categoria categoria = null;
 		try {
-			categoria = categoriaDAO.findById(id);
-			if (categoria != null)
-				log.info("AdminImplementation[buscarCategoriaporId()] ==> Encontrada la categoría: "
-					+ categoria.toString());
-			else
-				log.error("");
-
+			if (id != null) {
+				categoria = categoriaDAO.findById(id);
+				if (categoria == null)
+					log.info("[Info]AdminImplementation[buscarCategoriaPorId(<Long> id)] ==> No se encontró la categoría");
+				else
+					log.info("[Info]AdminImplementation[buscarCategoriaPorId(<Long> id)] ==> Encontrada la categoría: "
+							+ categoria.toString());
+			} else
+				log.info("[Error]AdminImplementation[buscarCategoriaPorId(<Long> id)] ==> id = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar la categoría con el id: "
+			log.error("[Error]AdminImplementation[buscarCategoriaPorId(<Long> id)] ==> No se pudo encontrar la categoría con el id: "
 					+ id);
 			throw e;
 		}
@@ -149,12 +171,18 @@ public class AdminImplementation implements AdminService {
 	public Categoria buscarCategoriaPorNombre(String nombre) {
 		Categoria categoria = null;
 		try {
-			categoria = categoriaDAO.findByNombre(nombre);
-			log.info("AdminImplementation ==> Encontrada la categoría: "
-						+ categoria.toString());
-
+			if (nombre != null) {
+				categoria = categoriaDAO.findByNombre(nombre);
+				if (categoria == null)
+					log.info("[Info]AdminImplementation[buscarCategoriaPorNombre(<String> nombre)] ==> No se encontró la categoría con el nombre: "
+							+ nombre);
+				else
+					log.info("[Info]AdminImplementation[buscarCategoriaPorNombre(<String> nombre)] ==> Encontrado la categoría: "
+							+ categoria.toString());
+			} else
+				log.error("[Error]AdminImplementation[buscarCategoriaPorNombre(<String> nombre)] ==> nombre = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar la categoría: "
+			log.error("[Error]AdminImplementation[buscarCategoriaPorNombre(<String> nombre)] ==> No se pudo encontrar la categoría con el nombre: "
 					+ nombre);
 			throw e;
 		}
@@ -165,10 +193,9 @@ public class AdminImplementation implements AdminService {
 		List<Categoria> categorias = null;
 		try {
 			categorias = categoriaDAO.findFirstLevel();
-			log.info("AdminImplementation ==> Lista de categorías principales encontrada correctamente");
-
+			log.info("[Info]AdminImplementation[buscarCategoriasPrincipales()] ==> Lista de categorías principales encontradas correctamente");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar ninguna categoría");
+			log.error("[Error]AdminImplementation[buscarCategoriasPrincipales()] ==> No se pudo encontrar ninguna categoría");
 			throw e;
 		}
 		return categorias;
@@ -177,11 +204,13 @@ public class AdminImplementation implements AdminService {
 	public List<Categoria> buscarSubcategorias(Categoria miCategoria) {
 		List<Categoria> subCategorias = null;
 		try {
-			subCategorias = categoriaDAO.findSubcategories(miCategoria);
-			log.info("AdminImplementation ==> Lista de subcategorías encontrada correctamente");
-
+			if (miCategoria != null){
+				subCategorias = categoriaDAO.findSubcategories(miCategoria);
+				log.info("[Info]AdminImplementation[buscarSubcategorias(<Clase> categoria)] ==> Lista de subcategorías encontrada correctamente");
+			}else
+				log.error("[Error]AdminImplementation[buscarSubcategorias(<Clase> categoria)] ==> categoria = null");
 		} catch (DataAccessException e) {
-			log.error("[Error]AdminImplementation ==> No se pudo encontrar ninguna categoría");
+			log.error("[Error]AdminImplementation[buscarSubcategorias(<Clase> categoria)] ==> No se pudo encontrar ninguna subcategoría");
 			throw e;
 		}
 		return subCategorias;
@@ -196,5 +225,4 @@ public class AdminImplementation implements AdminService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
